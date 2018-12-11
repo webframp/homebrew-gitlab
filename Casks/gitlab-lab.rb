9c27bc79c3181aa55f02aa3beab682c8c886cc22
Sean Escriva <sean.escriva@gmail.com>
2018-12-11 16:24:55 -0500

    Bump lab version to 0.14.0

--- Casks/gitlab-lab.rb
+++ Casks/gitlab-lab.rb
@@ -1,11 +1,10 @@
 cask 'gitlab-lab' do
-  version '0.10.1'
-  sha256 '7a215ca4b039403fd127edc02f6b9fdf58a044a3c925bd4d0e1666d8dfef977c'
+  version '0.14.0'
+  sha256 'cee443b499e617366abd2b9e9e7fcc8260dba445c98aa6fae0ef017890680c51'
 
   # Github releases url has been verified at the time of creating cask
   url "https://github.com/zaquestion/lab/releases/download/v#{version}/lab_#{version}_darwin_amd64.tar.gz"
-  appcast 'https://github.com/zaquestion/lab/releases.atom',
-          checkpoint: '7096ee3f7ebd4495794a54479d236410471d760f6265a86322699bc40eec8f7e'
+  appcast 'https://github.com/zaquestion/lab/releases.atom'
   name 'lab'
   homepage 'https://zaquestion.github.io/lab'
 
